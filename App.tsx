2e3f125e0f20a0b8182854ff272143a09f6c72da
AlessiaMoretti <nkolevepu@gmail.com>
2026-01-15 11:31:32 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import { FormData, Routine, Language } from './types';
 import { generateRoutine } from './routineGenerator';
 import { getTranslation } from './translations';
-import RoutineForm from './src/RoutineForm';
-import RoutineDisplay from './src/RoutineDisplay';
+import RoutineForm from "./src/RoutineForm";
+import RoutineDisplay from "./src/RoutineDisplay";
 import { Sparkles, Globe } from 'lucide-react';
 
 function App() {
