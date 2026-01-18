68189e6e8cb6b309cc415153b662886202ad29a4
AlessiaMoretti <nkolevepu@gmail.com>
2026-01-18 15:11:25 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import RoutineForm from "./RoutineForm";
+import RoutineDisplay from "./RoutineDisplay";
 
 export default function App() {
+  const routine = null; // временно - докато свържем данните
+
   return (
     <div style={{ padding: 24, fontFamily: "sans-serif" }}>
       <h1>Health & Beauty Routine Builder</h1>
+
       <RoutineForm />
+
+      <div style={{ marginTop: 24 }}>
+        {/* Важно: подаваме routine като prop, но засега е null */}
+        <RoutineDisplay routine={routine} />
+      </div>
     </div>
   );
 }
